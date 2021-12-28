840aa167dfa57b0cdb774c3b5484f44535a0ab87
Tetiana Pavlova <pavlovatania97@Gmail.com>
2021-12-28 17:35:49 +0300

    output of posts to the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { NgModule, Provider } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Provider} from '@angular/core';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {registerLocaleData} from '@angular/common';
+import ruLocale from '@angular/common/locales/ru';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PostPageComponent } from './post-page/post-page.component';
-import { PostComponent } from './shared/components/post/post.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
+import {PostComponent} from './shared/components/post/post.component';
+import {AuthInterceptor} from './shared/auth.interceptor';
 import { SharedModule } from './shared/shared.modules';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './shared/auth.interceptor';
+import { environment } from 'src/environments/environment';
+
+
+registerLocaleData(ruLocale, 'ru')
+
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
@@ -23,7 +30,7 @@ const INTERCEPTOR_PROVIDER: Provider = {
     MainLayoutComponent,
     HomePageComponent,
     PostPageComponent,
-    PostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
@@ -33,4 +40,6 @@ const INTERCEPTOR_PROVIDER: Provider = {
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
